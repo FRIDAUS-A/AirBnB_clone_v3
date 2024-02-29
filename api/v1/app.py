b43706fc2e470d292d753f61157918dd3f0b5fc4
FRIDAUS <fridausokoya@gmail.com>
2024-02-29 12:47:44 +0100

    status

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Starts a Flask Web app"""
 from flask import Flask
 from models import storage
 import os
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""initialize the app_view"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
