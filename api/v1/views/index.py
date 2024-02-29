19729faae038d93583dca7bfcdf9f9c5b170b070
FRIDAUS <fridausokoya@gmail.com>
2024-02-29 13:48:06 +0100

    stat

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 """Create views"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
 
 
 @app_views.route("/status", methods=["GET"])
@@ -9,3 +16,20 @@ def status():
     """Status of your API"""
     status = {'status': 'OK'}
     return jsonify(status)
+
+
+@app_views.route("/stats", methods=["GET"])
+def stats():
+    """stats of each objects"""
+    stat = {}
+    objs = {
+        "city": City,
+        "amenities": Amenity,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User,   
+    }
+    for key, value in objs.items():
+        stat[key] = storage.count(value)
+    return jsonify(stat)
