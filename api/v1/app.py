aa11d5d9c89f7fa1367ff27f4b5906fe78e382d4
FRIDAUS-A <fridausokoya@gmail.com>
2024-04-18 15:44:32 +0100

    upfate

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """Starts a Flask Web app"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 import os
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": ["0.0.0.0"]}})
 
 
 @app.teardown_appcontext
@@ -15,6 +17,11 @@ def teardown_appcontext(exception):
     """Closes the database again at the end of the request."""
     storage.close()
 
+@app.errorhandler(404)
+def handle_exception(e):
+    response = jsonify({"message": "Not found"})
+    response.status_code = 404
+    return response
 
 if __name__ == '__main__':
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
